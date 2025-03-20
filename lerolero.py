a7fb2a54dbc92eeb12b4c91b0852bd96e6475cce
Mathias <davi78569@usp.br>
2025-03-20 11:03:08 -0300

    aaaa

--- lerolero.py
+++ lerolero.py
@@ -4,6 +4,7 @@ Gera frases de efeito sem significado real"""
 
 import random
 
+<<<<<<< HEAD
 # listas de possibilidades para cada uma das partes.
 
 parte1 = [
@@ -25,3 +26,15 @@ parte3 = [
 ]
 
 print(random.choice(parte1), random.choice(parte2), random.choice(parte3))
+=======
+parte1 = []
+parte2 = []
+parte3 = []
+
+lingua = int(input("Escolha a lingua 1- portugês; 2 - Inglês\n"))
+if lingua == 2:
+	parte1 = ["aaa"]
+	parte2 = []
+	parte3 = []
+  print(random.choice(parte1), random.choice(parte2), random.choice(parte3))
+>>>>>>> ingles
