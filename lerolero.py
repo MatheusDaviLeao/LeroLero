c522adcb58f4a78d620ad43ce16d9093a29c2bd6
Pedro Gasan <73201013+pedrogasan@users.noreply.github.com>
2025-03-20 10:50:50 -0300

    Update lerolero.py
    Adição dos textos para randomização

--- lerolero.py
+++ lerolero.py
@@ -4,8 +4,24 @@ Gera frases de efeito sem significado real***
 
 import random
 
-parte 1 = ["a"]
-parte 2 = []
-parte 3 = []
+# listas de possibilidades para cada uma das partes.
 
-  print(random.choice(parte1), random.choice(parte2), random.choice(parte3))
+parte1 = [
+  "O sistema em desenvolvimento",
+  "O novo protocolo de comunicação",
+  "O algoritmo atualizado"
+]
+
+parte2 = [
+  "possui excelente desempenho",
+  "oferece garantias de precisão acima da média",
+  "preenche uma lacuna significativa"
+]
+
+parte3 = [
+  "as aplicações a que se destinam",
+  "em relação às opções disponíveis no mercado",
+  "provendo ampla vantagem competitiva a seu usuário"
+]
+
+print(random.choice(parte1), random.choice(parte2), random.choice(parte3))
