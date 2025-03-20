1d727ec64c005050242552c410ea619683fce1fd
MatheusDaviLeao <167720211+MatheusDaviLeao@users.noreply.github.com>
2025-03-20 11:01:54 -0300

    Update lerolero.py

--- lerolero.py
+++ lerolero.py
@@ -10,7 +10,7 @@ parte3 = []
 
 lingua = int(input("Escolha a lingua 1- portugês; 2 - Inglês\n"))
 if lingua == 2:
-	parte1 = ["aaa"]
+	parte1 = []
 	parte2 = []
 	parte3 = []
   print(random.choice(parte1), random.choice(parte2), random.choice(parte3))
