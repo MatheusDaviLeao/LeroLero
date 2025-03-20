e9607f0d7605744bc5352f654158939d9ae89de6
Mathias <davi78569@usp.br>
2025-03-20 10:43:46 -0300

    lerolero Inglês

--- lerolero.py
+++ lerolero.py
@@ -4,8 +4,13 @@ Gera frases de efeito sem significado real***
 
 import random
 
-parte 1 = ["a"]
-parte 2 = []
-parte 3 = []
+parte1 = []
+parte2 = []
+parte3 = []
 
+lingua = int(input("Escolha a lingua 1- portugês; 2 - Inglês\n"))
+if lingua == 2:
+	parte1 = []
+	parte2 = []
+	parte3 = []
   print(random.choice(parte1), random.choice(parte2), random.choice(parte3))
