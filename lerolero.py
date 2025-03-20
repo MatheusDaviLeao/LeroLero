8726b0726e1dd0b0db7a54f46b8aa45f88bb2ece
MatheusDaviLeao <167720211+MatheusDaviLeao@users.noreply.github.com>
2025-03-20 10:34:12 -0300

    Update lerolero.py

--- lerolero.py
+++ lerolero.py
@@ -1,3 +1,11 @@
 ***Gerador de Lero-lero
 
 Gera frases de efeito sem significado real***
+
+import random
+
+parte 1 = []
+parte 2 = []
+parte 3 = []
+
+  print(random.choice(parte1), random.choice(parte2), random.choice(parte3))
