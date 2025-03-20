51fb9066f9ef6a79598c7eec759224adb5791411
MatheusDaviLeao <167720211+MatheusDaviLeao@users.noreply.github.com>
2025-03-20 11:05:05 -0300

    Update lerolero.py

--- lerolero.py
+++ lerolero.py
@@ -25,16 +25,12 @@ parte3 = [
   "provendo ampla vantagem competitiva a seu usuário"
 ]
 
-print(random.choice(parte1), random.choice(parte2), random.choice(parte3))
-=======
-parte1 = []
-parte2 = []
-parte3 = []
-
 lingua = int(input("Escolha a lingua 1- portugês; 2 - Inglês\n"))
 if lingua == 2:
-	parte1 = ["aaa"]
+	parte1 = []
 	parte2 = []
 	parte3 = []
-  print(random.choice(parte1), random.choice(parte2), random.choice(parte3))
->>>>>>> ingles
+
+print(random.choice(parte1), random.choice(parte2), random.choice(parte3))
+
+
